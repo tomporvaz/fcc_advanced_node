8beab4cc539ad4178fade722e3a4f20a17f8fe3e
tomporvaz <tomporvaz@gmail.com>
2019-07-28 19:25:46 -0400

    Updated dependency and index route to use pug.

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug');
   });
 
 app.listen(process.env.PORT || 3000, () => {
