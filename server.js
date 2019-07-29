6da580ef1c3a68d50439587ecbd71d5a130b2dc4
tomporvaz <tomporvaz@gmail.com>
2019-07-28 20:32:01 -0400

    Added passport and express-session.  Mounted each in server.js.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -11,11 +13,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/')
   .get((req, res) => {
     res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please login'});
   });
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
